b8ef927d750c5a2c6a04a03b032f94f6ba3fb601
BreathlessWay <731005087@qq.com>
2020-05-28 18:47:26 +0800

    chore: 表单

--- lib/form/index.dart
+++ lib/form/index.dart
@@ -6,13 +6,49 @@ class FormScreen extends StatefulWidget {
 }
 
 class FormScreenState extends State<StatefulWidget> {
+  TextEditingController _controller = new TextEditingController();
+
+  @override
+  void initState() {
+    super.initState();
+    _controller.addListener(() {
+      print(_controller.text);
+    });
+  }
+
+  @override
+  void dispose() {
+    // TODO: implement dispose
+    _controller.dispose();
+    super.dispose();
+  }
+
   @override
   Widget build(BuildContext context) {
     return new Scaffold(
       appBar: new AppBar(
         title: new Text('表单'),
       ),
-      body: new Column(),
+      body: new Container(
+        padding: EdgeInsets.all(15.0),
+        child: new Column(
+          children: <Widget>[
+            new TextFormField(
+              controller: _controller,
+              decoration: new InputDecoration(border: new OutlineInputBorder()),
+            ),
+            new RaisedButton(
+                child: const Text('点我'),
+                onPressed: () => showDialog(
+                      context: context,
+                      child: AlertDialog(
+                        title: Text('Alert'),
+                        content: Text('You typed ${_controller.text}'),
+                      ),
+                    ))
+          ],
+        ),
+      ),
     );
   }
 }
