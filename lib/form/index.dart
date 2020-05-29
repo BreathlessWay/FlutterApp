0b7e5e7c5042c46bb2fcef4df69200743d6fbccd
BreathlessWay <731005087@qq.com>
2020-05-29 14:15:03 +0800

    feat: 平台区分

--- lib/form/index.dart
+++ lib/form/index.dart
@@ -6,7 +6,11 @@ class FormScreen extends StatefulWidget {
 }
 
 class FormScreenState extends State<StatefulWidget> {
+  final formKey = GlobalKey<FormState>();
+
   TextEditingController _controller = new TextEditingController();
+
+  String _email;
 
   @override
   void initState() {
@@ -18,9 +22,27 @@ class FormScreenState extends State<StatefulWidget> {
 
   @override
   void dispose() {
-    // TODO: implement dispose
     _controller.dispose();
     super.dispose();
+  }
+
+  void handleSubmit(BuildContext context) {
+    bool valid = formKey.currentState.validate();
+    print(valid);
+    print(_email);
+    if (valid) {
+      formKey.currentState.save();
+      print(_email);
+    }
+    if (Theme.of(context).platform == TargetPlatform.iOS) {
+      showDialog(
+        context: context,
+        child: AlertDialog(
+          title: Text('Alert'),
+          content: Text('You typed ${_controller.text}'),
+        ),
+      );
+    }
   }
 
   @override
@@ -29,26 +51,28 @@ class FormScreenState extends State<StatefulWidget> {
       appBar: new AppBar(
         title: new Text('表单'),
       ),
-      body: new Container(
-        padding: EdgeInsets.all(15.0),
-        child: new Column(
-          children: <Widget>[
-            new TextFormField(
-              controller: _controller,
-              decoration: new InputDecoration(border: new OutlineInputBorder()),
+      body: new Form(
+          key: formKey,
+          child: new Container(
+            padding: EdgeInsets.all(15.0),
+            child: new Column(
+              children: <Widget>[
+                new TextFormField(
+                  validator: (value) =>
+                      !value.contains('@') ? 'Not a valid email.' : null,
+                  onSaved: (val) => _email = val,
+                  decoration: new InputDecoration(
+                      icon: new Icon(Icons.map),
+                      border: new OutlineInputBorder(),
+                      hintText: '请输入',
+                      labelText: '输入框：'),
+                ),
+                new RaisedButton(
+                    child: const Text('点我'),
+                    onPressed: () => handleSubmit(context))
+              ],
             ),
-            new RaisedButton(
-                child: const Text('点我'),
-                onPressed: () => showDialog(
-                      context: context,
-                      child: AlertDialog(
-                        title: Text('Alert'),
-                        content: Text('You typed ${_controller.text}'),
-                      ),
-                    ))
-          ],
-        ),
-      ),
+          )),
     );
   }
 }
